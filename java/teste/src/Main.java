5db2709438dac4e439fef57c4126a4752915a551
zLeonardoIshida <leoishida@hotmail.com>
2023-06-28 22:33:35 -0300

    ik

--- java/teste/src/Main.java
+++ java/teste/src/Main.java
@@ -1,5 +1,8 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
+import java.util.List;
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
@@ -8,7 +11,26 @@ public class Main {
 
         Prova.printar();
         Prova p = new Prova(5);
-        p.printar();
+        try {
+            p.erro(p.getNum());
+        }
+        catch (Exception e) {
+            System.out.println("entrei no catch");
+            System.out.println(e.getMessage());
+        }
+
+        Fila<Integer> f = new Fila(10);
+        f.add(1);
+        f.add(2);
+        f.add(3);
+        f.add(4);
+        f.printa();
+        f.printa();
+
+        Generica.nsei(15);
+        Generica.nsei("Chuchu com abobrinha");
+
+
     }
      
 }
@@ -21,8 +43,49 @@ class Prova {
         this.num = num;
     }
 
+    public int getNum() {
+        return this.num;
+    }
+
     public static void printar() {
         System.out.println("acessei um metodo estico");
     }
 
+    public void erro(int num) throws Exception {
+        if (num == 5) {
+            throw new Exception("Deu pau", null);
+        }
+        else {
+            System.out.println("Deu bom");
+        }
+    }
+}
+
+class Fila<T> {
+    private int tam;
+    private int inicio;
+    private int fim;
+    public List<T> fila;
+
+    public Fila(int tam) {
+        this.tam = tam;
+        this.fila = new ArrayList<T>(tam);
+        this.inicio = 0;
+        this.fim = -1;
+    }
+
+    public void printa() {
+        System.out.println(this.fila.get(inicio++));
+    }
+
+    public void add(T toAdd) {
+        this.fim++;
+        fila.add(toAdd);
+    }
+}
+
+class Generica {
+    public static <T> void nsei(T param) {
+        System.out.println(param);
+    }
 }
